1c6b7406e10969a1e77b6e57f6f751bd697b8245
Codemis <johnathan@pulos.com>
2022-01-18 11:24:59 -0800

    Added asArray and asJson methods to simplify exporting to JSON files.

--- src/Exporter/Models/Episode.php
+++ src/Exporter/Models/Episode.php
@@ -38,6 +38,13 @@ class Episode
     public $image = '';
 
     /**
+     * The remote URL to download the image.
+     *
+     * @var string
+     */
+    public $imageUrl = '';
+
+    /**
      * The path to the local image of the collection
      *
      * @var string
@@ -57,6 +64,13 @@ class Episode
      * @var string
      */
     public $mimeType = '';
+
+    /**
+     * The remote URL for the resource
+     *
+     * @var string
+     */
+    public $resourceUrl = '';
 
     /**
      * The slug of the episode.
@@ -85,17 +99,21 @@ class Episode
      * @param string $slug The slug for the Episode
      * @param string $title The title for the Episode
      * @param string $desc The description for the Episode
+     * @param string $imageUrl The url for the image
      * @param string $mediaType The type of media
      * @param string $localFilename The local file's path
      * @param string $localImage The local image's path
+     * @param string $resourceUrl The url for the resource
      */
     public function __construct(
         string $slug,
         string $title,
         string $desc,
+        string $imageUrl,
         string $mediaType,
         string $localFilename,
-        string $localImage
+        string $localImage,
+        string $resourceUrl
     ) {
         if (! file_exists($localImage)) {
             throw new \InvalidArgumentException('The episode image does not exist!');
@@ -106,12 +124,14 @@ class Episode
         $this->slug = $slug;
         $this->title = $title;
         $this->desc = $desc;
+        $this->imageUrl = $imageUrl;
         $this->mediaType = $mediaType;
         $this->localImage = $localImage;
         $this->image = basename($localImage);
         $this->localFilename = $localFilename;
         $this->filename = basename($localFilename);
         $this->mimeType = mime_content_type($localFilename);
+        $this->resourceUrl = $resourceUrl;
     }
 
     /**
@@ -124,5 +144,54 @@ class Episode
         if (! \in_array($tag, $this->tags, true)) {
             $this->tags[] = $tag;
         }
+    }
+
+    /**
+     * Get an array for this object
+     *
+     * @param bool $isMainFile Is this for the main file?
+     * @param bool $isSlim Is this for the slim packaging?
+     * @return array The array of the object
+     */
+    public function asArray(bool $isMainFile = false, bool $isSlim = false): array
+    {
+        $data = [
+            'desc' => $this->desc,
+            'filename' => $this->filename,
+            'image' => $this->image,
+            'mediaType' => $this->mediaType,
+            'mimeType' => $this->mimeType,
+            'slug' => $this->slug,
+            'tags' => $this->tags,
+            'title' => $this->title
+        ];
+        if ($isMainFile) {
+            unset($data['desc']);
+            unset($data['filename']);
+            unset($data['image']);
+            unset($data['mediaType']);
+            unset($data['mimeType']);
+            unset($data['slug']);
+            unset($data['tags']);
+        }
+        if ($isSlim) {
+            $data['imageUrl'] = $this->imageUrl;
+            $data['resourceUrl'] = $this->resourceUrl;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Get a JSON string for this object
+     *
+     * @param bool $isMainFile Is this for the main file?
+     * @param bool $isSlim Is this for the slim packaging?
+     * @return string The JSON string
+     */
+    public function asJson(bool $isMainFile = false, bool $isSlim = false): string
+    {
+
+        return json_encode($this->asArray($isMainFile, $isSlim), \JSON_UNESCAPED_UNICODE);
     }
 }
